2880208fcd63c399f259f959b072775e9ec46892
Ernestine Lahey <ernestine.lahey@hva.nl>
2024-06-29 17:09:09 +0200

    First commit

---
+++ .idea/server/logger.js
@@ -0,0 +1,15 @@
+// logger.js
+import { createLogger, format, transports } from "winston";
+
+const logger = createLogger({
+    level: 'info', // Adjust the log level as needed
+    format: format.combine(
+        format.colorize(),
+        format.simple()
+    ),
+    transports: [
+        new transports.Console(),
+    ],
+});
+
+export {logger}
