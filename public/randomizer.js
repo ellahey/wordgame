b4655758afb1e679ce7221429a20ecd0fe2cd525
Ernestine Lahey <ernestine.lahey@hva.nl>
2024-07-02 20:01:01 +0200

    Incorporates falling animation

--- public/randomizer.js
+++ public/randomizer.js
@@ -1,16 +1,32 @@
+const container = document.getElementById('letters-container');
 let intervalId;
 
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
+
+function getRandomLetter() {
+    const randomIndex = Math.floor(Math.random() * ALPHABET.length);
+    return ALPHABET[randomIndex];
+}
+
+function renderLetter(letter) {
+    const letterElement = document.createElement('div');
+    letterElement.className = 'letter';
+    letterElement.innerText = letter;
+    letterElement.style.paddingLeft = `${Math.random() * 100}vw`;
+    letterElement.style.animationDuration = `${Math.random() * 5 + 3}s`; // Random fall duration between 3 and 8 seconds
+    container.appendChild(letterElement);
+
+    // Remove the letter after it falls out of view
+    letterElement.addEventListener('animationend', () => {
+        letterElement.remove();
+    });
+}
+
 function startRandomLetters() {
-    const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
-
-    function getRandomLetter() {
-        const randomIndex = Math.floor(Math.random() * ALPHABET.length);
-        const randomLetter = ALPHABET[randomIndex];
-        console.log(randomLetter);
-    }
-
-    // Start the interval to run getRandomLetter every 100 milliseconds
-    intervalId = setInterval(getRandomLetter, 100);
+    intervalId = setInterval(() => {
+        const letter = getRandomLetter();
+        renderLetter(letter);
+    }, 100);
 }
 
 function stopRandomLetters() {
@@ -25,3 +41,4 @@ setTimeout(stopRandomLetters, 5000);
 
 // Expose the stop function to the global scope for manual stopping
 window.stopRandomLetters = stopRandomLetters;
+
