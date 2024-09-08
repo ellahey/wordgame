ce4a6b4accc8ff4d75237eb6750efe3ebf744374
Ernestine Lahey <ernestine.lahey@hva.nl>
2024-09-08 20:16:28 +0200

    refactored

--- public/randomizer.mjs
+++ public/randomizer.mjs
@@ -1,5 +1,6 @@
 class RaindropApp {
     constructor(levelTimeout, levelWordLength) {
+        // Initialization code remains the same
         this.timeout = levelTimeout;
         this.container = document.getElementById('letters-container');
         this.selectedLetterContainer = document.getElementById('selection-container');
@@ -10,9 +11,9 @@ class RaindropApp {
         this.intervalId = null;
         this.ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         this.letterArray = [];
-        this.raindrops = []; // Array to keep track of raindrops
-        this.isRainStopped = false; // Flag to track if rain has stopped
-        this.onRainStop = null; // Callback function when rain stops
+        this.raindrops = [];
+        this.isRainStopped = false;
+        this.onRainStop = null;
         this.wordLength = levelWordLength;
         this.rearrangeArea = null;
         this.buttonArea = null;
@@ -21,7 +22,9 @@ class RaindropApp {
         this.instructions = document.getElementById('instructions');
         this.levelTitle = document.getElementById('levelTitle');
         this.MAX_LETTERS = 6;
+        this.dragging = false; // Flag to check if a letter is being dragged
     }
+
     getRandomLetter() {
         const randomIndex = Math.floor(Math.random() * this.ALPHABET.length);
         return this.ALPHABET[randomIndex];
@@ -35,18 +38,15 @@ class RaindropApp {
         raindrop.style.animationDuration = `${Math.random() * 5 + 3}s`;
         this.container.appendChild(raindrop);
         this.raindrops.push(raindrop);
-if (this.raindrops.length >= this.MAX_LETTERS) {
-    raindrop.addEventListener('click', () => this.renderSelectedLetter(letter));
-} else {
-    this.message.innerText = 'You have already selected 7 letters.'
-}
+
+        raindrop.addEventListener('click', () => this.renderSelectedLetter(letter));
 
         raindrop.addEventListener('animationend', () => {
             raindrop.remove();
-            this.raindrops = this.raindrops.filter(drop => drop !== raindrop); // Remove from array
-            // Check if all raindrops have stopped
+            this.raindrops = this.raindrops.filter(drop => drop !== raindrop);
+
             if (this.raindrops.length === 0 && this.isRainStopped && this.onRainStop) {
-                this.onRainStop(); // Invoke callback function
+                this.onRainStop();
             }
         });
     }
@@ -57,7 +57,6 @@ if (this.raindrops.length >= this.MAX_LETTERS) {
             this.renderLetter(letter);
         }, 100);
 
-        // Stop the random letter generator after the specified timeout
         setTimeout(() => {
             this.stopRain();
             this.isRainStopped = true;
@@ -72,9 +71,9 @@ if (this.raindrops.length >= this.MAX_LETTERS) {
         this.letterArray.push(letter);
         const span = document.createElement('span');
         span.innerText = letter;
-        span.draggable = true // Make the span draggable
-        span.addEventListener('dragstart', this.handleDragStart); // Add dragstart event listener
-        span.id = `letter-${letter}-${Date.now()}`; // Unique ID
+        span.draggable = true;
+        span.addEventListener('dragstart', this.handleDragStart);
+        span.id = `letter-${letter}-${Date.now()}`;
         this.selectedLetterElement.appendChild(span);
     }
 
@@ -84,21 +83,19 @@ if (this.raindrops.length >= this.MAX_LETTERS) {
 
     renderLetterSpaces() {
         this.rearrangeArea = document.getElementById('rearrange-area');
-        this.rearrangeArea.innerHTML = ''; // Clear previous spaces
-        let totalWidth = 0;
+        this.rearrangeArea.innerHTML = '';
 
         for (let i = 0; i < this.wordLength; i++) {
             const space = document.createElement('div');
             space.className = 'letter-space';
-            space.setAttribute('data-index', `${i}`); // Store index for reference
-            space.innerText = '_'; // Placeholder visual representation
+            space.setAttribute('data-index', `${i}`);
+            space.innerText = '_';
             space.addEventListener('dragover', this.handleDragOver);
             space.addEventListener('drop', this.handleDrop);
             this.rearrangeArea.appendChild(space);
-            totalWidth += space.offsetWidth; // Add the width of each space
         }
 
-        this.wordArea.style.display = 'block'; // Show wordArea
+        this.wordArea.style.display = 'block';
     }
 
     setInstructionText(text) {
@@ -126,13 +123,17 @@ if (this.raindrops.length >= this.MAX_LETTERS) {
                 this.clearInstructionText();
                 this.startRain();
             }, 5000);
-        }, 3000)
+        }, 3000);
     }
-
 
     // Drag and Drop Event Handlers
     handleDragStart = (event) => {
-        const target = event.target; // selected letter
+        if (this.dragging) {
+            event.preventDefault(); // Prevent dragging if another letter is already being dragged
+            return;
+        }
+        this.dragging = true; // Set dragging flag
+        const target = event.target;
         event.dataTransfer.setData('text/plain', target.innerText);
         event.dataTransfer.setData('source-id', target.id);
     }
@@ -143,46 +144,49 @@ if (this.raindrops.length >= this.MAX_LETTERS) {
 
     handleDrop = (event) => {
         event.preventDefault();
+        this.dragging = false; // Reset dragging flag
 
-        // Get the letter being dragged
         const letter = event.dataTransfer.getData('text/plain');
         const sourceId = event.dataTransfer.getData('source-id');
         const draggedElement = document.getElementById(sourceId);
 
-        // Check if dropping on a letter-space
+        // If dropped on a letter-space
         if (event.currentTarget.className === 'letter-space') {
-            // If the target space already contains a letter, exit early
             if (event.currentTarget.innerText !== '_') {
-                return;
+                // Swap letters if the target space is occupied
+                const currentLetter = event.currentTarget.innerText;
+                const currentSourceId = event.currentTarget.getAttribute('data-source-id');
+
+                // Move current letter back to selection area
+                const previousElement = document.getElementById(currentSourceId);
+                previousElement.style.visibility = 'visible';
+
+                // Replace the existing letter with the new one
+                event.currentTarget.innerText = letter;
+                event.currentTarget.setAttribute('data-source-id', sourceId);
+                draggedElement.style.visibility = 'hidden';
+            } else {
+                // Place the letter in the empty space
+                event.currentTarget.innerText = letter;
+                event.currentTarget.setAttribute('data-source-id', sourceId);
+                draggedElement.style.visibility = 'hidden';
             }
-
-            // Place the letter in the target space and hide the dragged letter
-            event.currentTarget.innerText = letter;
-            draggedElement.style.visibility = 'hidden';
-            event.currentTarget.setAttribute('data-source-id', sourceId); // Store the source ID on the space for future reference
-
-        } else {
-            // Handle if the letter is being dragged back to the selected letters container
-            const selectedLetterContainer = document.getElementById('selection-container');
-            if (selectedLetterContainer.contains(event.target)) {
-                // Find the space that originally held the letter
-                const spaces = document.querySelectorAll('.letter-space');
-                spaces.forEach(space => {
-                    if (space.getAttribute('data-source-id') === sourceId) {
-                        space.innerText = '_'; // Clear the letter space
-                        space.removeAttribute('data-source-id'); // Remove the reference
-                    }
-                });
-
-                // Make the letter visible again
-                draggedElement.style.visibility = 'visible';
-            }
+        } else if (event.currentTarget === this.selectedLetterContainer) {
+            // If dropped back in the selection area
+            const spaces = document.querySelectorAll('.letter-space');
+            spaces.forEach(space => {
+                if (space.getAttribute('data-source-id') === sourceId) {
+                    space.innerText = '_';
+                    space.removeAttribute('data-source-id');
+                }
+            });
+            draggedElement.style.visibility = 'visible';
         }
     }
 
     renderButton() {
-        console.log('renderButton called')
-        this.buttonArea = document.createElement('div')
+        console.log('renderButton called');
+        this.buttonArea = document.createElement('div');
         this.buttonArea.className = 'button-area';
         const button = document.createElement('button');
         button.type = 'submit';
@@ -193,17 +197,16 @@ if (this.raindrops.length >= this.MAX_LETTERS) {
         button.addEventListener('click', () => this.extractWord());
     }
 
-    // If no letters or not enough letters for level have been selected:
     showReTryMessage(text) {
-        this.setInstructionText(text)
-        const retryButton = document.createElement('button')
+        this.setInstructionText(text);
+        const retryButton = document.createElement('button');
         retryButton.type = 'button';
         retryButton.id = 'retry';
         retryButton.addEventListener('click', () => this.extractWord());
     }
 
     extractWord() {
-        console.log('extractWord() is called')
+        console.log('extractWord() is called');
         let userWord = '';
         const children = this.rearrangeArea.children;
         for (let i = 0; i < children.length; i++) {
@@ -217,20 +220,20 @@ if (this.raindrops.length >= this.MAX_LETTERS) {
 
     postWord(word) {
         console.log('postWord function called');
-        const lowercaseWord = word.toLowerCase(); // Convert to lowercase
+        const lowercaseWord = word.toLowerCase();
         fetch('/api/dictionary', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ word: lowercaseWord }) // Send lowercase word
+            body: JSON.stringify({ word: lowercaseWord })
         }).then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
             return response.json();
         }).then(data => {
-            this.renderResult(data.code)
+            this.renderResult(data.code);
             console.log(data);
         }).catch(error => {
             console.error('Error:', error);
@@ -239,42 +242,27 @@ if (this.raindrops.length >= this.MAX_LETTERS) {
 
     renderResult(code) {
         if (code === 0) {
-            this.message.innerText = 'You made a word!'
+            this.message.innerText = 'You made a word!';
         } else if (code === 1) {
-            this.message.innerText = 'Word not found in dictionary.'
+            this.message.innerText = 'Word not found in dictionary.';
         }
     }
-
 }
 
+// Level initialization code remains the same
 
+const levelOne = new RaindropApp(5000, 3);
+const levelOneTitle = 'Word Rain - Level One';
+const levelOneText = 'Welcome to WordRain. This is level one. To win this round, you must make a valid 3-letter word. When the rain starts to fall, click on the drops to select a letter. You can select a maximum of 7 letters. Good luck!';
 
-/* Level One ****************************************************************************************/
-    const levelOne = new RaindropApp(5000, 3);
-    const levelOneTitle = 'Word Rain - Level One';
-    const levelOneText = 'Welcome to WordRain. This is level one. To win this round, you must make a valid 3-letter' +
-        'word. When the rain starts to fall, click on the drops to select a letter. You can select a maximum of 7 letters.' +
-        '\r\n Good luck!'
-
-
-//TODO - move function below to inside class
-    levelOne.setOnRainStop(() => {
-        console.log('Rain has stopped. Rendering additional elements.');
-        if(levelOne.letterArray.length > 0) {
-            levelOne.renderLetterSpaces();
-            levelOne.renderButton();
-        }
-        // Display message or instructions for user
-        //TODO - create a separate class for handling the area / messages below
-        levelOne.message.innerText = 'Drag letters to spaces to form a word, then click "Submit"';
-        levelOne.message.className = 'instruction-message';
-        levelOne.wordArea.appendChild(levelOne.message);
-    });
-    levelOne.initialize(levelOneTitle, levelOneText);
-
-
-/* Level Two  /****************************************************************************************/
-/* Level Three /***************************************************************************************/
-/* Lightening Round  /*********************************************************************************/
-
-
+levelOne.setOnRainStop(() => {
+    console.log('Rain has stopped. Rendering additional elements.');
+    if (levelOne.letterArray.length > 0) {
+        levelOne.renderLetterSpaces();
+        levelOne.renderButton();
+    }
+    levelOne.message.innerText = 'Drag letters to spaces to form a word, then click "Submit"';
+    levelOne.message.className = 'instruction-message';
+    levelOne.wordArea.appendChild(levelOne.message);
+});
+levelOne.initialize(levelOneTitle, levelOneText);
