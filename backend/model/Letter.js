92b358d9efaec7b8f5b2290b6a8dbc90f649fc2e
Ernestine Lahey <ernestine.lahey@hva.nl>
2024-09-21 08:38:29 +0200

    Refactors

--- backend/model/Letter.js
+++ backend/model/Letter.js
@@ -1,21 +1,25 @@
 class Letter {
 
     constructor() {
-        this.name = Letter.generateLetter()
+        this.character = Letter.generateLetter()
         this.id = Letter.incrementId()
     }
 
     static generateLetter() {
         const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         const randomIndex = Math.floor(Math.random() * ALPHABET.length);
-        name = ALPHABET[randomIndex];
-        return name;
+        let character = ALPHABET[randomIndex];
+        return character;
     }
 
     static incrementId() {
         if (!this.latestId) this.latestId = 1
         else this.latestId++
         return this.latestId
+    }
+
+    getCharacter() {
+        return this.character;
     }
 
 }
--- backend/model/Word.js
+++ backend/model/Word.js
@@ -1,9 +1,21 @@
 class Word {
-
-    constructor(length) {
-        this.length = ;
-        this.letter = letter;
+    constructor(letters) {
+        this.letters = letters.map(letter => new Letter(letter));
+        this.length = this.letters.length;
     }
 
-    getLength
+    getWord() {
+        return this.letters.map(letter => letter.getCharacter()).join('');
+    }
+
+    getLength(letters) {
+        let count;
+        letters.forEach((letter) => {
+            if (letter) {
+                count++;
+            }
+        })
+        return this.length
+    }
+
 }
